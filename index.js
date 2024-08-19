3e847affa60317e1534aeba32d6906a882d98ae9
Vy Pham <vyp@provokesolutions.com>
2024-08-19 01:08:56 -0500

    first attempt to be a cool engineer like louis

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const subtractionTest = [{valOne: 1, valTwo: 1, answer: 0},{valOne: 77, valTwo: 
 const multiplicationTest = [{valOne: 1, valTwo: 1, answer: 1},{valOne: 77, valTwo: 77, answer: 5929}, {valOne: 123, valTwo: 567, answer: 69741}]
 const divisionTest = [{valOne: 1, valTwo: 1, answer: 1},{valOne: 77, valTwo: 77, answer: 1}, {valOne: 123, valTwo: 567, answer: .2169312169312169}]
 
+function add(valOne, valTwo) {
+    return valOne + valTwo;
+}
+
+function subtraction(valOne, valTwo) {
+    return valOne + valTwo;
+}
+
+function multiplication(valOne, valTwo) {
+    return valOne * valTwo;
+}
+
+function division(valOne, valTwo) {
+    if (valTwo === 0) {
+        throw new Error("You dumb or what?")
+    }
+    return valOne / valTwo;
+}
 
 
 additionTest.map((test) => {
@@ -15,7 +33,7 @@ subtractionTest.map((test) => {
     if (result != test.answer) throw new Exception(`Addition for ${test.valOne} - ${test.valTwo} recieved ${result} expected ${test.answer}`)
 })
 
-multplicationTest.map((test) => {
+multiplicationTest.map((test) => {
     const result = multiplication(test.valOne, test.valTwo)
     if (result != test.answer) throw new Exception(`Addition for ${test.valOne} * ${test.valTwo} recieved ${result} expected ${test.answer}`)
 })
@@ -24,3 +42,5 @@ divisionTest.map((test) => {
     const result = division(test.valOne, test.valTwo)
     if (result != test.answer) throw new Exception(`Addition for ${test.valOne} / ${test.valTwo} recieved ${result} expected ${test.answer}`)
 })
+
+
